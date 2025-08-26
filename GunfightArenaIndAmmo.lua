4eb64d7014fdf62f22cf4cf6b02f16892f5a135c
Script Hub <151373064+Korruptedchild@users.noreply.github.com>
2025-08-26 22:59:58 +0100

    Update GunfightArenaIndAmmo.lua

--- GunfightArenaIndAmmo.lua
+++ GunfightArenaIndAmmo.lua
@@ -1,1 +1,14 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Weapons = ReplicatedStorage:WaitForChild("Weapons")
 
+for _, weapon in pairs(Weapons:GetChildren()) do
+    if weapon:FindFirstChild("Variables") then
+        local vars = weapon.Variables
+        if vars:FindFirstChild("Ammo") then
+            vars.Ammo.Value = 8383828
+        end
+        if vars:FindFirstChild("StoredAmmo") then
+            vars.StoredAmmo.Value = 8383828
+        end
+    end
+end
