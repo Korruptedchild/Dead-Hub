c1facffc223485daffdfb5ddf8ca036068f9422b
Script Hub <151373064+Korruptedchild@users.noreply.github.com>
2025-09-18 00:12:46 +0100

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,9 +1,1 @@
-return(function(...)
-local f={
-    "\112\114\105\110\116\040\034\104\101\121\034\041"
-}
-local s = table.concat(f, '')
-local t={f=73383;Z=-244781;h=263171}
-for F=1,3 do local _ = F*2 end
-load(s)()
-end)()
+return(function(...)local a={"\112\114\105\110\116\040\034\104\101\121\034\041"}local b=table.concat(a,'')local c={f=73383,Z=-244781,h=263171}for d=1,3 do local e=d*2 end;load(b)()end)()
